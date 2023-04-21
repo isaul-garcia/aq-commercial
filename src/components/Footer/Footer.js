48e575c8da178881b50c0b976889f6a539f94149
Isaúl García <56583387+isaul-garcia@users.noreply.github.com>
2023-04-21 15:48:11 -0400

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,9 +16,9 @@ const Footer = () => {
             <FooterContainer>
                 <SocialMedia>
                     <SocialMediaWrap>
-                        <SmallText><a style={{textDecoration: 'none', color: '#1e1e1e'}} href="mailto:aqcommercial@gmail.com">aqcommercial@gmail.com</a></SmallText>
+                        <SmallText><a style={{textDecoration: 'none', color: '#1e1e1e'}} href="mailto:aqviews1@gmail.com">aqviews1@gmail.com</a></SmallText>
                         <SocialIcons>
-                            <SocialIconLink href="https://www.instagram.com/aqcommercial/" target="_blank" aria-label="Instagram">
+                            <SocialIconLink href="https://www.instagram.com/aqviews/" target="_blank" aria-label="Instagram">
                                 <FaInstagram />
                             </SocialIconLink>
                         </SocialIcons>
